fe72a921a3d85779cda7162e4b96f0ba37873ecc
Hacker <jimpig38@yahoo.com.tw>
2017-08-09 15:01:20 +0800

    0609
    Register Nontest

---
+++ RegReturn.php
@@ -0,0 +1,31 @@
+<?php
+    header("Content-Type:text/html; charset=utf-8");
+
+    $a = $_POST["account"];
+    
+    $con = mysqli_connect('localhost','hacker','jimpig38');
+
+    mysqli_query($con,"set character set utf8");
+    mysql_query("SET CHARACTER_SET_database= utf8",$con);
+    mysql_query("SET CHARACTER_SET_CLIENT= utf8",$con);
+    mysql_query("SET CHARACTER_SET_RESULTS= utf8",$con);
+
+    mysqli_select_db($con,'wardrobe');
+    $sql = "SELECT * FROM user_id WHERE account_id = '".$a."' ";
+    $result = mysqli_query($con,$sql);
+    $hint = "";
+    $ret = "此帳號可使用";
+    while($row = mysqli_fetch_array($result)){
+        if($a==$row[account]){
+            $ret = "此帳號已存在";
+        }
+    }
+        
+    
+/*
+    echo "<Script>";
+    echo "getFile(".json_string.");";
+    echo "/<Script>";
+*/
+    echo $json_string;
+?>

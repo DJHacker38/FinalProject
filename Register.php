40179994501b780fff00a631fcbbf603d6fd4895
Hacker <jimpig38@yahoo.com.tw>
2017-08-24 05:48:12 +0800

    0824
    account checkable

--- Register.php
+++ Register.php
@@ -1,7 +1,15 @@
 <?php
     header("Content-Type:text/html; charset=utf-8");
 
-    $a = $_POST["account"];
+    $name = $_POST["name"];
+    $account = $_POST["account"];
+    $password = $_POST["password"];
+    $phone = $_POST["phone"];
+    $address = $_POST["address"];
+    $height = $_POST["height"];
+    $weight = $_POST["weight"];
+    $email = $_POST["email"];
+
     
     $con = mysqli_connect('localhost','hacker','jimpig38');
 
@@ -11,8 +19,9 @@
     mysql_query("SET CHARACTER_SET_RESULTS= utf8",$con);
 
     mysqli_select_db($con,'wardrobe');
-    $sql = "SELECT * FROM user_id WHERE account_id = '".$a."' ";
-    $result = mysqli_query($con,$sql);
+    $sql = "INSERT INTO `wardrobe`.`user_id` (`name`, `account_id`, `password`, `phonenumber`, `address`, `high`, `weight`, `email`) VALUES ($name, $account, $password, $phone, $address, $height, $weight, $email);"; //改成Create語法
+    mysqli_query($con,$sql);
+/*
     $hint = "";
     $ret = "此帳號可使用";
     while($row = mysqli_fetch_array($result)){
@@ -20,6 +29,10 @@
             $ret = "此帳號已存在";
         }
     }
-        
+  
+
     echo $ret;
+
+*/
+    echo "";
 ?>
